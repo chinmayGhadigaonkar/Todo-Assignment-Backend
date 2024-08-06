b6a1383d539d9a59d8c19d4bb0aa50961ec2e07e
chinmayGhadigaonka <ghadigaonkarchinmay@gmail.com>
2024-08-06 10:39:08 +0530

    cors

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ const app = express();
 
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://todo-assignment-front-end.vercel.app/",
-    "*",
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, //access-control-allow-credentials:true
 };
 
 app.use(cors(corsOptions));
